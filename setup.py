be5301b408633ccce0569a85586e07f419c58128
Ahmet Erkan Turan <erkan.turan@tum.de>
2025-05-28 15:10:31 +0200

    add requirements

--- setup.py
+++ setup.py
@@ -4,6 +4,15 @@ setup(
     name="lnlp_project",
     version="0.1",
     packages=find_packages(),  # Automatically includes dataset/, utils/ etc.
+    install_requires=[
+        "tensorflow",
+        "torch",
+        "transformers",
+        "pandas",
+        "scikit-learn",
+        "numpy",
+        "matplotlib",
+    ],
 )
 
 # run pip install -e . to install the package in editable mode - to use absolute imports
